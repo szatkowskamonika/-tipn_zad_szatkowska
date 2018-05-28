c08d57e95eb4be5b1dbb8a9a251227499fb07ed7
szatkowskamonika <38323603+szatkowskamonika@users.noreply.github.com>
2018-05-28 11:25:01 +0200

    zadania5

---
+++ zadania5.R
@@ -0,0 +1,29 @@
+library(readr)
+movies <- read_csv("movies.csv")
+View(movies)
+
+library(tidyverse)
+
+#zadanie 5-2
+
+filter(movies, year == 2005, Comedy == 1)
+
+# zadanie 5-3
+
+select(movies, title, year, budget) %>% 
+  arrange(desc(budget))
+
+#zadanie 5-4
+
+filter(movies, Animation == 1, year < 2000 & year >=1990 ) %>%
+  arrange(desc(rating))
+
+#zadanie 5-5
+
+filter(movies, Drama == 1) %>% 
+  arrange(movies, desc(length))
+
+#zadanie 5-6
+
+group_by(movies, mpaa) %>%
+  summarise (sr = mean(rating), odch_stand= mad(rating))
